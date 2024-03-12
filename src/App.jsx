e14a0dfb50ce250cc2cd79c005764b6af27ff506
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-03-12 15:04:54 -0700

    added modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import { useState } from 'react';
+
+import About from './About';
 import BestBooks from './BestBooks';
 import BookForm from './BookForm';
-import About from './About';
+import Header from './Header';
+import Footer from './Footer';
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
@@ -11,33 +16,42 @@ import {
   Route
 } from "react-router-dom";
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <nav>
-        </nav>
-        <Router>
-          <Header />
-          <Routes>
-            <Route 
-              exact path="/"
-              element={<BestBooks />}
-            >
-            </Route>
-            <Route 
-              exact path="/about"
-              element={<About />}
-            >
-            </Route>
-          </Routes>
-          <BookForm>
-          </BookForm>
-          <Footer />
-        </Router>
-      </>
-    )
-  }
+function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+    <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Book Form</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <BookForm />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<BestBooks />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
+
+      <Button variant="primary" onClick={handleShow}>
+        Open Form
+      </Button>
+    </>
+  );
 }
 
 export default App;
